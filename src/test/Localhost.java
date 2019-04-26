2c92de08fa287541b29806f31f678a249cd34e4d
bit <bit@DESKTOP-HS3D1O4>
2019-04-26 17:01:40 +0900

    first commit

---
+++ src/test/Localhost.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Localhost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			
+			String hostname = inetAddress.getHostName();
+			String hostAddress = inetAddress.getHostAddress();
+			
+			System.out.println(hostname + ":" + hostAddress);
+		
+			byte[] addresses = inetAddress.getAddress();
+			for(byte address : addresses) {
+				System.out.print(address & 0x000000ff);
+				System.out.print(".");
+			}
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
